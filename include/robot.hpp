3527e06d9255ac6bd27928539fa63793f206392e
Rishabh Mukund <rishabh.m96@gmail.com>
2021-10-15 13:52:13 -0400

    Initialising the Robot Header File

---
+++ include/robot.hpp
@@ -0,0 +1,98 @@
+/**
+ * @file robot.hpp
+ * @author Rishabh Mukund (rmukund@umd.edu)
+ * @author Prannoy Namala (pnamala@umd.edu)
+ * @brief Header file for Robot
+ * @version 0.1
+ * @date 10-15-2021
+ *
+ * Copyright (c) 2021 Group MT14
+ *
+ */
+
+#include <iostream>
+#include <stdexcept>
+
+enum double pos[3];
+
+class Robot {
+ public:
+    /**
+     * @brief Construct a new robot object
+     *
+     * @param max_acc (double) - Maximum Acceleration
+     * @param wheel_base (double) - Wheel base
+     * @param track_width (double) - Track Width
+     */
+    Robot(double max_acc, double wheel_base, double track_width);
+
+    /**
+    * @brief Get the max_acc parameter
+    *
+    * @return double
+    */
+    double getMaxAcc();
+
+    /**
+    * @brief Get the wheel_base parameter
+    *
+    * @return double
+    */
+    double getWheelBase();
+
+    /**
+    * @brief Get the track_width parameter
+    *
+    * @return double
+    */
+    double getTrackWidth();
+
+    /**
+    * @brief Get the curr_pos parameter
+    *
+    * @return pos
+    */
+    pos getCurrPos();
+
+    /**
+    * @brief Set the curr_pos parameter
+    *
+    * @param curr_pos (pos) - Current Position (x,y,th)
+    */
+    void setCurrPos(pos curr_pos);
+
+    /**
+    * @brief Get the curr_vel parameter
+    *
+    * @return double
+    */
+    double getCurrVel();
+
+    /**
+    * @brief Set the curr_vel parameter
+    *
+    * @param curr_vel (double) - Current Velocity
+    */
+    void setCurrVel();
+
+    /**
+    * @brief Get the final_pos parameter
+    *
+    * @return pos
+    */
+    pos getFinalPos();
+
+    /**
+    * @brief Set the final_pos parameter
+    *
+    * @param final_pos (pos) - Final Position (x,y,th)
+    */
+    void setFinalPos(pos final_pos);
+
+ private:
+    double _max_acc, _wheel_base, _track_width;
+    double _curr_vel{};
+    pos final_pos = {0,0,0}, curr_pos = {0,0,0};
+};
+
+
